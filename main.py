e5b41ce82d025fb0b37816b4a63ac7f1175d9e17
lantianxing <lantianxing@vipkid.com.cn>
2020-02-17 10:56:41 +0800

    add rule

--- main.py
+++ main.py
@@ -5,25 +5,34 @@ from stock import Stock
 from rule import PriceRule
 from define import *
 from sendEmail import sendEmail
+import time
 
-def value_get(code):
-    slice_num, value_num = 21, 3
-    name, now = u'——无——', u'  ——无——'
-    if code in ['s_sh000001', 's_sz399001']:
-        slice_num = 23
-        value_num = 1
-    r = requests.get("http://hq.sinajs.cn/list=%s" % (code,))
-    res = r.text.split(',')
-    print(json.dumps(res))
-    if len(res) > 1:
-        name, now = res[0][slice_num:], res[value_num]
-    return name + ' ' + now
+rules = [
+    ['博通集成', 'sh603068', 120, 102],
+    ['湖南黄金', 'sz002155', 8, 7.0],
+    ['科技ETF', 'sh515000', 1.5, 1.2],
+    ['欧菲光', 'sz002456', 18.6, 15],
+    ['100红利', 'sh515180', 1.2, 0.93],
+    ['贝瑞基因', 'sz000710',42, 39.3],
+    ['半导体', 'sh512480', 2.26, 1.6],
+    ['金发科技', 'sh600143', 9.6, 8],
+]
 
+def check():
+    for rule in rules:
+        stock = Stock(rule[0], rule[1])
+        rule = PriceRule(rule[2], rule[3])
+        ret = stock.fitPrice(rule)
+        if (ret['code'] == CODE_SEND_EMAIL):
+            sendEmail(ret['msg'], 'aaaaa')
+        print(ret)
 
 if(__name__=='__main__'):
-    stock = Stock('ST慧业', 'sz000816')
-    rule = PriceRule(1.1, 1.0)
-    ret = stock.fitPrice(rule)
-    if(ret['code'] == CODE_SEND_EMAIL):
-        sendEmail(ret['msg'], 'aaaaa')
-    print(ret)
+    while(True):
+        check()
+        # break
+        time.sleep(300)
+
+
+
+
